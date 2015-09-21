8f06b217081e9d3965482562abd207ecc5dcb2df
stevenlundy <steven.lundy@gmail.com>
2015-09-21 16:45:01 -0700

    Refactor soundController into RhythmGeneratorController Implement Sound Playback

--- client/app/rhythm-generator/rhythm-generator.js
+++ client/app/rhythm-generator/rhythm-generator.js
@@ -9,10 +9,20 @@ app.controller('RhythmGeneratorController', function($scope, $timeout) {
     };
   }
 
+  $scope.sounds = ['hihat','snare','kick','none'];
+  $scope.soundFiles = {};
+  for (var i = 0; i < $scope.sounds.length; i++) {
+    var audioEl = new Audio();
+    audioEl.src = '/assets/samples/' + $scope.sounds[i] + '.mp3';
+    $scope.soundFiles[$scope.sounds[i]] = audioEl;
+  }
+
   playNext = function() {
     if($scope.playing){
       $scope.active = ($scope.active + 1) % 16;
-      //Somehow play audio file
+      var sound = $scope.steps[$scope.active].sound;
+      console.log(sound);
+      $scope.soundFiles[sound].play();
       $timeout(playNext, 60000/$scope.tempo);
     } else {
       $scope.active = null;
