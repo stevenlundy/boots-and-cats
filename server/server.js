307ce312aba590a3c7355d04cc76383728036e9f
stevenlundy <steven.lundy@gmail.com>
2015-09-21 16:07:37 -0700

    Change listening statement

--- server/server.js
+++ server/server.js
@@ -10,5 +10,5 @@ var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('listening at http://%s:%s', host, port);
 });
