2865ed4be9e072a3a31a05dc2a64360840bd7b3d
stevenlundy <steven.lundy@gmail.com>
2015-09-21 13:26:45 -0700

    Add tempo slider

--- client/app/rhythm-generator/rhythm-generator.js
+++ client/app/rhythm-generator/rhythm-generator.js
@@ -1,5 +1,6 @@
 app.controller('RhythmGeneratorController', function($scope){
   $scope.steps = [];
+  $scope.tempo = 100;
   for(var i = 0; i < 16; i++){
     $scope.steps[i] = {
       sound: 'none'
