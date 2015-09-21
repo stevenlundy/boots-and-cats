9f9397ee9bc109cbf1825c4ba0894894ce68e4a3
stevenlundy <steven.lundy@gmail.com>
2015-09-21 11:36:51 -0700

    Set up basic server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var port = 3000;
+
+var app = express();
+
+app.use(express.static(__dirname + '/../client'));
+
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
