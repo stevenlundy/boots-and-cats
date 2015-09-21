bb64cfaa9c12cd7e6fad01706004fc1caae8b3d7
stevenlundy <steven.lundy@gmail.com>
2015-09-21 14:44:39 -0700

    Add prescale setting to form

--- client/app/rhythm-generator/rhythm-generator.js
+++ client/app/rhythm-generator/rhythm-generator.js
@@ -1,6 +1,7 @@
 app.controller('RhythmGeneratorController', function($scope){
-  $scope.steps = [];
   $scope.tempo = 100;
+  $scope.prescale = "_4_4";
+  $scope.steps = [];
   for(var i = 0; i < 16; i++){
     $scope.steps[i] = {
       sound: 'none'
