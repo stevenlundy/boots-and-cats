4aec9b2bc568300375aac3ffa423b55ee12d8660
stevenlundy <steven.lundy@gmail.com>
2015-09-21 12:23:48 -0700

    Add controller functionality and binding for beats in rhythm generator controller

--- client/app/rhythm-generator/rhythm-generator.js
+++ client/app/rhythm-generator/rhythm-generator.js
@@ -1,3 +1,8 @@
 app.controller('RhythmGeneratorController', function($scope){
-  
+  $scope.beats = [];
+  for(var i = 0; i < 16; i++){
+    $scope.beats[i] = {
+      sound: 'none'
+    };
+  }
 });
