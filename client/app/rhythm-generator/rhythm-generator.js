4ca9f551913edcefb4d0ead566c52711e3feb07d
stevenlundy <steven.lundy@gmail.com>
2015-09-21 15:06:57 -0700

    Add styling for active step in rhythm

--- client/app/rhythm-generator/rhythm-generator.js
+++ client/app/rhythm-generator/rhythm-generator.js
@@ -4,7 +4,8 @@ app.controller('RhythmGeneratorController', function($scope){
   $scope.steps = [];
   for(var i = 0; i < 16; i++){
     $scope.steps[i] = {
-      sound: 'none'
+      sound: 'none',
+      active: false
     };
   }
 });
